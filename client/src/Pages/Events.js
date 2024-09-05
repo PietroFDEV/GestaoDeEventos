2b63b433de0b785d4ec4684565ca5a1c330bd480
Lucas Martins <lucasluh.morais@hotmail.com>
2024-09-05 20:36:12 -0300

    events filter

--- client/src/Pages/Events.js
+++ client/src/Pages/Events.js
@@ -1,7 +1,43 @@
+import Header from '../Components/Header.js'
+import Footer from '../Components/Footer.js'
+import '../Styles/Events.css'
+
 function Events(){
     return(
-        <div>
-            <h1>Events</h1>
+        <div className="events-page">
+            <Header logged={false}></Header>
+            <div className="filter-div">
+                <div className="filter-div-category">
+                    <p>Categoria</p>
+                    <div className='category-options'>
+                        <p>teste</p>
+                    </div>
+                </div>
+                <div className="filter-div-category">
+                    <p>Categoria</p>
+                    <div className='category-options'>
+                        <p>teste</p>
+                    </div>
+                </div>
+                <div className="filter-div-category">
+                    <p>Categoria</p>
+                    <div className='category-options'>
+                        <p>teste</p>
+                    </div>
+                </div>
+                <div className="filter-div-category">
+                    <p>Categoria</p>
+                    <div className='category-options'>
+                        <p>teste</p>
+                    </div>
+                </div>
+                <div className="filter-div-category">
+                    <p>Categoria</p>
+                    <div className='category-options'>
+                        <p>teste</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
